5f898ba42fed54ba5a5896f9c083942719f6a415
dieselleo <diesel.leonardo@gmail.com>
2021-05-01 14:12:17 +0100

    Going to play football on fairview

--- js/customers.js
+++ js/customers.js
@@ -1,13 +1,20 @@
-function getCustomers(){
-    let output = '<h2>Users</h2>'
-    fetch('https://randomuser.me/api/?results=5')
+function getCustomers() {
+  var output = "<h1>test1</h1>"
+  fetch('https://randomuser.me/api/?results=5')
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      data.forEach(function(user){
-        output += `<h1>test</h1>`;
+      (data.results).forEach(user => {
+        Object.entries(user).forEach(function(){
+          console.log("foi")
+          output = `<div class="data-container column" style="background-color: blue;">
+          <h1 id="name">${name.first}</h1><br>
+          <h2 id="add1">${location.street}</h2>
+          <h2 id="add2">${location.city}</h2></div>`
+          $(document).ready(function (){
+            document.getElementById("customers").innerHTML = output
+          })
+        })        
       })
-    },
-    document.getElementById('customers').innerHTML = output
-    )
-    }
+    });
+}
